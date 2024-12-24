2291194b99a8b031bdf603b899a25e6db12261c7
Roee Hashai <roee.hashai@gmail.com>
2024-12-24 14:35:51 +0200

    added logging prints

--- resolver.py
+++ resolver.py
@@ -17,25 +17,25 @@ s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.bind(('', int(PORT)))
 
 while True:
-    # print("[Cache] Before request: ", cache)
-    # print("[Queue] Before request: ", queue)
+    print("[Cache] Before request: ", cache)
+    print("[Queue] Before request: ", queue)
     # Receive the domain name from the client decode it and strip it
     data, addr = s.recvfrom(1024)
     data = data.decode().strip()
-    # print(f"[Request] Received {data} from {addr}")
+    print(f"[Request] Received {data} from {addr}")
     
     # clear old data from cache
     cur_time = datetime.datetime.now()
     while queue and queue[0][1] < cur_time:
         cache.pop(queue.popleft()[0], None)
         
-    # print("[Cache] After clearing: ", cache)
-    # print("[Queue] After clearing: ", queue)
+    print("[Cache] After clearing: ", cache)
+    print("[Queue] After clearing: ", queue)
     # check if data in cache and send it if able to send it without resolving
     if data in cache and cache[data][1] != 'NS':
-        # print(f"[Cache] Found {data} in cache, not resolving")
+        print(f"[Cache] Found {data} in cache, not resolving")
         domain, (ip, record_type) = data, cache[data]
-        # print(f"[Cache] Sending {ip} to {addr}")
+        print(f"[Cache] Sending {ip} to {addr}")
         s.sendto(ip.encode(), addr)
         continue
     
@@ -49,18 +49,18 @@ while True:
     if best_match:                                  # if a suffix was found, use it to resolve
         ip, port = cache[best_match][0].split(':')
         port = int(port)
-        # print(f"[Cache] Found {best_match} in cache, resolving {data} with {best_match}")
+        print(f"[Cache] Found {best_match} in cache, resolving {data} with {best_match}")
     
     # send domain to DNS server and loop until resolved
     resolved = False
     while not resolved:
-        # print(f"[Resolving] Sending {data} to {ip}:{port}")
+        print(f"[Resolving] Sending {data} to {ip}:{port}")
         s.sendto(data.encode(), (ip, port))
         response, _ = s.recvfrom(1024)
         if(response.decode().strip() == 'non-existent domain'):
             cache[data] = ('non-existent domain', 'NE')
             queue.append((data, datetime.datetime.now() + time_delta))
-            # print(f"[Resolving] Sending 'non-existent domain' to {addr}")
+            print(f"[Resolving] Sending 'non-existent domain' to {addr}")
             s.sendto('non-existent domain'.encode(), addr)
             break
         domain, ip, record_type = response.decode().strip().split(',')
@@ -69,7 +69,7 @@ while True:
         cache[domain] = (ip, record_type)
         if record_type == 'A':
             resolved = True
-            # print(f"[Resolving] Sending {ip} to {addr}")
+            print(f"[Resolving] Sending {ip} to {addr}")
             s.sendto(ip.encode(), addr)
         else:
             ip, port = ip.split(':')
